ae4fa7d3f323760523b955a8735923cc9d6bef56
coomanio0837 <coomanio0837@gmail.com>
2022-04-12 21:27:19 +0300

    main code

---
+++ quest.js
@@ -0,0 +1,78 @@
+let a = '';
+let b = '';
+let sign = '';
+let End = false;
+
+
+const number = [ '0','1','2','3','4','5','6','7','8','9','.'];
+const act = [ '*','/','+','-',];
+
+var screen = document.getElementById('screen');
+
+
+function clearscreen(){
+    var screen = document.getElementById('screen');
+    screen.value = "";
+    a = '';
+    b = '';
+    sign = '';
+    End = false;
+}
+
+function btn(mark){
+    var screen = document.getElementById('screen');
+if (number.includes(mark)) {
+    if(b === "" && sign ===""){
+        a+=mark;
+
+        screen.value = a;
+    }
+    else if (a!== "" && b!=="" && End === true){
+        b = mark;
+        End = false;
+        screen.value = b;
+    }
+    else{
+        b+= mark;
+        screen.value = b;
+    }
+    return
+ }
+if(act.includes(mark)){
+    sign = mark;
+    screen.value = sign;
+    return;
+}
+if(mark === '='){
+    switch (sign) {
+        case "*":
+            a = a * b;
+            break;
+        case "/":
+            if(b === "0"){
+                screen.value = "error";
+                a = '';
+                b = '';
+                sign = '';
+                return;
+            }
+            a = a / b;
+            break;
+        case "+":
+            a = (+a) + (+b);
+            break;
+        case "-":
+            a = a - b;
+            break;
+    }
+    End = true;
+    screen.value = a;
+  }
+}
+function square(){
+b = '';
+a = a*a;
+screen.value = a;
+}
+
+
